c6d46b1efebbad3a8de9c924b72c3d377df6583a
Aditya Chaudhary <chaudharyaditya.in@gmail.com>
2020-07-29 17:05:19 +0530

    Changing the source to Covid19india.org
    Changing the source to Covid19india.org

--- papp.py
+++ papp.py
@@ -8,7 +8,7 @@ import time
 import requests
 import datetime
 from bs4 import BeautifulSoup
-import sqlite3
+import json
 
 # sys.stdin = open('input.in', 'r')  
 # sys.stdout = open('output.out', 'w')
@@ -18,6 +18,85 @@ from sqlalchemy import Column,Integer,String,Date,desc
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 db= SQLAlchemy(app)
+def APIUSE():
+	URL = 'https://api.covid19india.org/states_daily.json'
+	response_json=requests.get(URL)
+	response_json=response_json.json()
+	states_daily=response_json['states_daily']
+	many_keys=states_daily[0].keys()
+	total_Active=0
+	total_Confirmed=0
+	total_Recovered=0
+	total_Deceased=0
+	new__dict_Confirmed=dict()
+	new__dict_Recovered=dict()
+	new__dict_Deceased=dict()
+	new_active_Cases=dict()
+	status_can_be=['Recovered','Confirmed','Deceased']
+	otherthan_numer=['status','date','tt']
+	for k in many_keys:
+		if k not in otherthan_numer:
+			new__dict_Deceased[k]=0
+			new__dict_Recovered[k]=0
+			new__dict_Confirmed[k]=0
+			new_active_Cases[k]=0
+		else:
+			if k=='date':
+				new__dict_Confirmed[k]='FINAL'
+				new__dict_Deceased[k]='FINAL'
+				new__dict_Recovered[k]='FINAL'
+				new_active_Cases[k]='PRESENT'
+
+	for day in states_daily:
+		if day['status']=='Confirmed':
+			for ke in many_keys:
+				if ke not in otherthan_numer:
+					# print(type(day[ke]),day[ke],ke)
+					new__dict_Confirmed[ke]+=int(day[ke])
+		if day['status']=='Recovered':
+			for ke in many_keys:
+				if ke not in otherthan_numer:
+					new__dict_Recovered[ke]+=int(day[ke])
+		if day['status']=='Deceased':
+			for ke in many_keys:
+				if ke not in otherthan_numer:
+					new__dict_Deceased[ke]+=int(day[ke])
+	for k in many_keys:
+		if k not in otherthan_numer:
+			new_active_Cases[k]=new__dict_Confirmed[k]-new__dict_Recovered[k]-new__dict_Deceased[k]
+			total_Deceased+=new__dict_Deceased[k]
+			total_Recovered+=new__dict_Recovered[k]
+			total_Active+=new_active_Cases[k]
+			total_Confirmed+=new__dict_Confirmed[k]
+	# print('CORONA VIRUS UPDATES IN INDIA')
+	# print("Active Cases",total_Active)
+	# print("Confirmed Cases",total_Confirmed)
+	# print("Recovered CASES",total_Recovered)
+	# print("Deceased Cases",total_Deceased)
+	# print("LATEST TREND PREVIOUS DAY")
+	# print("Deceased")
+	# print(states_daily[-1]['tt'])
+	# print("RECOVERED")
+	# print(states_daily[-2]['tt'])
+	# print("Confirmed")
+	# print(states_daily[-3]['tt'])
+	current=dict()
+	LATEST_Increase=dict()
+	total_Active="{:,}".format(total_Active)
+	
+	current['Active']=total_Active
+
+	current['Confirm']="{:,}".format(total_Confirmed)
+	current['Recovered']="{:,}".format(total_Recovered)
+	current['Deceased']="{:,}".format(total_Deceased)
+	LATEST_Increase['Confirm']="{:,}".format(int(states_daily[-3]['tt']))
+	LATEST_Increase['Recovered']="{:,}".format(int(states_daily[-2]['tt']))
+	LATEST_Increase['Deceased']="{:,}".format(int(states_daily[-1]['tt']))
+	LATEST_Increase['Active']="{:,}".format(int(states_daily[-3]['tt'])-int(states_daily[-2]['tt'])-int(states_daily[-1]['tt']))
+	returnobj=[current,LATEST_Increase]
+	return returnobj
+
+
 class CORONACASES(db.Model):
 	'''
 	table CORONACASES
@@ -40,66 +119,22 @@ class TOTCORONACASES(db.Model):
 
 @app.route("/update",methods=["POST","GET"])
 def update():
-	URL = 'https://www.mohfw.gov.in/index.html'
-	r=requests.get(URL)
-	soup=BeautifulSoup(r.content, 'html5lib')
-	conn= sqlite3.connect('test.db')
-	tbody=soup('tbody')
-	# print("Source https://www.mohfw.gov.in/index.html")
-	# print("S. No.","State","Numbers")
-	list_of_all_tr=tbody[-1]('tr')
-	intot=0
-	now = datetime.datetime.now()
-	# print ("UPDATED AT  date and time : ")
-	# print (now.strftime("%Y-%m-%d %H:%M:%S"))
-	for w in list_of_all_tr:
-		state=w('td')
-		if len(state)==5:
-			number=0
-			conn.execute('''INSERT OR REPLACE INTO CORONACASES(ID,NAMEOFSTATE,INDIANCASES,FOREIGNCASES,RECOVEREDCASES,DEATHCASES) VALUES(?,?,?,?,?,?);''',(int(state[0].contents[0]),str(state[1].contents[0]),int(state[2].contents[0]),0,int(state[3].contents[0]),int(state[4].contents[0])))
-			# print("inserted Success")
-			for val in state:
-				if number==2:
-					intot+=int(val.contents[0])
-				# print((val.contents[0]),end="     ")
-				number+=1
-			# print()
-		elif len(state)==4:
-			state=w('strong')
-			listtot=[]
-			intc=0
-			for val in state:
-				# print(val.contents[0],end=" ")
-				if intc==1:
-					strval=val.contents[0]
-					intval=""
-					for e in range(len(strval)):
-						if e!=len(strval)-1:
-							intval+=strval[e]
-					intval=int(intval)
-					listtot.append(intot)
-				elif intc!=0:
-					listtot.append(int(val.contents[0]))
-				intc+=1
-			# print(listtot)
-			conn.execute('''INSERT OR REPLACE INTO TOTCORONACASES(ID,INDIANCASES,FOREIGNCASES,RECOVEREDCASES,DEATHCASES) VALUES(?,?,?,?,?);''',(1,listtot[0],0,listtot[1],listtot[2]))		
-			# print()
-	qwer=listtot[0]
-	# print("indian confirmed total  ",intot)
-	conn.commit()
-	# print("commited")
-	conn.close()
-	case=CORONACASES.query.all()
-	desccase=CORONACASES.query.order_by(desc(CORONACASES.INDIANCASES)).all()
-	total=TOTCORONACASES.query.all()
-	return render_template("home.html",cases=case,totalcase=total,desccases=desccase,totindia=qwer)
+	abh=APIUSE()
+	current=abh[0]
+	LATEST_Increase=abh[1]
+
+
+	return render_template("home.html",Current=current,Latest_increase=LATEST_Increase)
 
 @app.route("/",methods=["POST","GET"])
 def home():
-	case=CORONACASES.query.all()
-	desccase=CORONACASES.query.order_by(desc(CORONACASES.INDIANCASES)).all()
-	total=TOTCORONACASES.query.all()
-	return render_template("home.html",cases=case,totalcase=total,desccases=desccase)
+	abh=APIUSE()
+	current=abh[0]
+	LATEST_Increase=abh[1]
+
+
+	return render_template("home.html",Current=current,Latest_increase=LATEST_Increase)
+
 @app.route("/info",methods=["POST","GET"])
 def info():
 	return render_template("info.html")
